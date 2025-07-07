580abc56e128db7c8818bfc5370659bef5cebffd
s0l0n3t <furkantokgz@gmail.com>
2025-07-07 11:06:45 +0300

    template added

---
+++ backend/renart-backend/src/main/java/com/furkantkgz/renartbackend/RenartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.furkantkgz.renartbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenartBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenartBackendApplication.class, args);
+    }
+
+}
---
+++ backend/renart-backend/src/test/java/com/furkantkgz/renartbackend/RenartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkantkgz.renartbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenartBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
