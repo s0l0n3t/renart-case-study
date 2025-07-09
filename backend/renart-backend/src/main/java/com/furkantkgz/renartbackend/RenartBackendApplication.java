6e56f17ed4e34fd8be4810ad5d49bebf412d5aee
s0l0n3t <furkantokgz@gmail.com>
2025-07-09 19:45:41 +0300

    cors webconfig added

--- backend/renart-backend/src/main/java/com/furkantkgz/renartbackend/RenartBackendApplication.java
+++ backend/renart-backend/src/main/java/com/furkantkgz/renartbackend/RenartBackendApplication.java
@@ -2,11 +2,13 @@ package com.furkantkgz.renartbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class RenartBackendApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RenartBackendApplication.class, args);
     }
 
